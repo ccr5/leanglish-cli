c72b915e3a58b4f199a930403e16d32dca8df52f
CCR5 <matt-gomes@live.com>
2023-06-11 12:49:16 -0300

    Exploring match using clap crate

--- src/leanglish_clap.rs
+++ src/leanglish_clap.rs
@@ -1,33 +1,29 @@
-use { 
-    clap::{
+pub mod leanglish_clap {
+
+    use clap::{
         crate_version, 
         crate_description,
         crate_name,crate_authors,
         Command,
         Arg
+    };
+
+    pub fn get_leanglish_clap() -> Command {
+        Command::new(crate_name!())
+            .author(crate_authors!())
+            .version(crate_version!())
+            .about(crate_description!())
+            .subcommand(
+                Command::new("connection")
+                    .arg(Arg::new("new"))
+                    .arg(Arg::new("is_connected"))
+            )
+            .subcommand(
+                Command::new("dict")
+                    .arg(Arg::new("list"))
+                    .arg(Arg::new("today"))
+
+            )
+            .arg(Arg::new("learn"))
     }
-};
-
-fn get_leanglish_clap() -> Command {
-    let commands = Command::new(crate_name!())
-        .author(crate_authors!())
-        .version(crate_version!())
-        .about(crate_description!())
-        .subcommand(
-            Command::new("new")
-                .arg(Arg::new("user"))
-                .arg(Arg::new("dict"))   
-        )
-        .subcommand(
-            Command::new("connection")
-                .arg(Arg::new("new"))
-                .arg(Arg::new)
-        )
-
-        .arg(Arg::new("new"))
-        .arg(Arg::new("connection"))
-        .arg(Arg::new("learn"));
-
-    commands
-        
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod leanglish_clap;
+
+use leanglish_clap::leanglish_clap::get_leanglish_clap;
+
 fn main() {
-    println!("Hello, world!");
+    let args = get_leanglish_clap().get_matches();
+    match args.subcommand() {
+        Some(("connection",  sub_m)) => { println!("{:?}", sub_m) }, // clone was used
+        Some(("dict",   sub_m)) => { println!("{:?}", sub_m) }, // push was used
+        _                       => { println!("Hello World") }, // Either no subcommand or one not tested 
+    }
 }
